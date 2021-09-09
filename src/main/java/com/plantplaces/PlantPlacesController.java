b20bc411080d6b9e83ab2cef42df23aa7b9a40f2
Austin Walder <46360340+AWalderDev@users.noreply.github.com>
2021-09-09 14:40:30 -0400

    Update / endpoints

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PlantPlacesController {
 	
+	
 	@RequestMapping("/start")
 	public String start() {
+		
+		return "start";
+	}
+	
+	@RequestMapping("/")
+	public String index() {
 		
 		return "start";
 	}
